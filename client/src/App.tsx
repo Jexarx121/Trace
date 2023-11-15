bf2e8f084ffe13eb0ee3b4574199a56122dffa76
Zhi <zjchen402@gmail.com>
2023-11-15 22:47:05 +0000

    Feat: Add links to register and login page. Add constants file too

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
-import { Homepage } from "./pages";
+import { Homepage, Login, Register } from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { LINKS } from "./components/constants";
 
 const App = () => {
   return (
-    <Homepage/>
+    <Router>
+      <Routes>
+        <Route path={LINKS.LOGIN} element={<Login/>}/>
+        <Route path={LINKS.REGISTER} element={<Register/>}/>
+        <Route path={LINKS.HOMEPAGE} element={<Homepage/>}/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ client/src/components/constants.ts
@@ -0,0 +1,5 @@
+export const LINKS = {
+  'REGISTER': '/register',
+  'HOMEPAGE': '/',
+  'LOGIN': '/login'
+};
