fbfe9956888a4f45abf7f8c16a11b0a39dac6453
Zhi <zjchen402@gmail.com>
2023-12-25 22:57:33 +0000

    feat: added supabase auth ui. Replaced register and form login from it

--- client/src/components/Authentication/Register/RegisterFormFields.tsx
+++ client/src/components/Authentication/Register/RegisterFormFields.tsx
@@ -1,111 +1,110 @@
-import RegisterSchema from "./RegisterSchema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { useState, useEffect } from "react";
-import { capitalize } from "../../../helpers/functions";
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import { supabase } from "../../../supabase/supabaseClient";
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+// import RegisterSchema from "./RegisterSchema";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { z } from "zod";
+// import { useForm } from "react-hook-form";
+// import { useState, useEffect } from "react";
+// import { capitalize } from "../../../helpers/functions";
+// import '@fortawesome/fontawesome-free/css/all.min.css';
 
-type RegisterSchemaType = z.infer<typeof RegisterSchema>;
+// type RegisterSchemaType = z.infer<typeof RegisterSchema>;
 
-const RegisterForm = () => {
-  const [showPassword, setShowPassword] = useState(false);
-  const [isSafeToReset, setIsSafeToReset] = useState(false);
+// const RegisterForm = () => {
+//   const [showPassword, setShowPassword] = useState(false);
+//   const [isSafeToReset, setIsSafeToReset] = useState(false);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset
-  } = useForm<RegisterSchemaType>({ resolver: zodResolver(RegisterSchema), mode: "onBlur"});
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//     reset
+//   } = useForm<RegisterSchemaType>({ resolver: zodResolver(RegisterSchema), mode: "onBlur"});
 
-  useEffect(() => {
-    if (!isSafeToReset) return;
+//   useEffect(() => {
+//     if (!isSafeToReset) return;
  
-    reset(); // asynchronously reset your form values
-  }, [reset])
+//     reset(); // asynchronously reset your form values
+//   }, [reset])
   
-  const onSubmit = async (userData: RegisterSchemaType) => {
-    const firstName = capitalize(userData.firstName);
-    const surname = capitalize(userData.surname);
-    const { age, email, password } = userData;
+//   const onSubmit = async (userData: RegisterSchemaType) => {
+//     const firstName = capitalize(userData.firstName);
+//     const surname = capitalize(userData.surname);
+//     const { age, email, password } = userData;
 
-    try {
-      const { data, error } = await supabase.auth.signUp({
-        email: email,
-        password: password,
-        options: {
-          data: {
-            first_name: firstName,
-            surname: surname,
-            age: age
-          }
-        }
-      });
-      console.log('Success!')
-      if (error) {
-        throw error;
-      }
-    } catch (error) {
-      console.log("Error occured", {error});
-    }
-  }; 
+//     try {
+//       const { data, error } = await supabase.auth.signUp({
+//         email: email,
+//         password: password,
+//         options: {
+//           data: {
+//             first_name: firstName,
+//             surname: surname,
+//             age: age
+//           }
+//         }
+//       });
+//       console.log('Success!')
+//       if (error) {
+//         throw error;
+//       }
+//     } catch (error) {
+//       console.log("Error occured", {error});
+//     }
+//   }; 
   
-  return (
-    <div className="flex md:flex-row flex-col">
-      <form className="flex flex-col p-5 pr-8 border-r border-gray-400" onSubmit={handleSubmit(onSubmit)}>
-        <label className='uppercase text-[#49A078] text-md'>First Name</label>
-        <input className='mb-1 mt-1 p-3 outline-1 outline-[#263228] outline rounded-sm pr-5 focus:outline-[#49A078] focus:outline-2'
-          {...register("firstName")} placeholder="John"/>
-        <span className="text-red-500 text-sm mt-0 mb-4">{errors.firstName?.message}</span>
+//   return (
+//     <div className="flex md:flex-row flex-col">
+//       <form className="flex flex-col p-5 pr-8 border-r border-gray-400" onSubmit={handleSubmit(onSubmit)}>
+        // <label className='uppercase text-[#49A078] text-md'>First Name</label>
+//         <input className='mb-1 mt-1 p-3 outline-1 outline-[#263228] outline rounded-sm pr-5 focus:outline-[#49A078] focus:outline-2'
+//           {...register("firstName")} placeholder="John"/>
+//         <span className="text-red-500 text-sm mt-0 mb-4">{errors.firstName?.message}</span>
 
-        <label className='uppercase text-[#49A078] text-md'>Surname</label>
-        <input className='mb-1 mt-1 p-3 outline-1 outline-[#263228] outline rounded-sm pr-5 focus:outline-[#49A078] focus:outline-2'
-          {...register("surname")} placeholder="Doe"/>
-        <span className="text-red-500 text-sm mt-0 mb-4">{errors.surname?.message}</span>
+//         <label className='uppercase text-[#49A078] text-md'>Surname</label>
+//         <input className='mb-1 mt-1 p-3 outline-1 outline-[#263228] outline rounded-sm pr-5 focus:outline-[#49A078] focus:outline-2'
+//           {...register("surname")} placeholder="Doe"/>
+//         <span className="text-red-500 text-sm mt-0 mb-4">{errors.surname?.message}</span>
 
-        <label className='uppercase text-[#49A078] text-md'>Age</label>
-        <input className='mb-1 mt-1 p-3 outline-1 outline-[#263228] outline rounded-sm pr-5 focus:outline-[#49A078] focus:outline-2'
-          {...register("age", {valueAsNumber: true})} placeholder="22"/> 
-        <span className="text-red-500 text-sm mt-0 mb-4">{errors.age?.message}</span>
+//         <label className='uppercase text-[#49A078] text-md'>Age</label>
+//         <input className='mb-1 mt-1 p-3 outline-1 outline-[#263228] outline rounded-sm pr-5 focus:outline-[#49A078] focus:outline-2'
+//           {...register("age", {valueAsNumber: true})} placeholder="22"/> 
+//         <span className="text-red-500 text-sm mt-0 mb-4">{errors.age?.message}</span>
 
-        <label className='uppercase text-[#49A078] text-md'>Email Address</label>
-        <input className='mb-1 mt-1 p-3 outline-1 outline-[#263228] outline rounded-sm pr-5 focus:outline-[#49A078] focus:outline-2'
-          {...register("email")} placeholder="john@example.com"/>
-        <span className="text-red-500 text-sm mt-0 mb-4">{errors.email?.message}</span>
+//         <label className='uppercase text-[#49A078] text-md'>Email Address</label>
+//         <input className='mb-1 mt-1 p-3 outline-1 outline-[#263228] outline rounded-sm pr-5 focus:outline-[#49A078] focus:outline-2'
+//           {...register("email")} placeholder="john@example.com"/>
+//         <span className="text-red-500 text-sm mt-0 mb-4">{errors.email?.message}</span>
 
-        <label className='uppercase text-[#49A078] text-md'>Password</label>
-        <div className="relative">
-          <input className='mb-1 mt-1 p-3 outline-1 outline-[#263228] outline rounded-sm pr-5 focus:outline-[#49A078] focus:outline-2'
-            {...register("password")} placeholder="Password" type={showPassword ? "text": "password"}/>
-            <span onClick={() => setShowPassword(!showPassword)}
-            className="cursor-pointer absolute right-2 top-4 text-[#263228]">
-              {/* Font Awesome Eye Icon */}
-              {showPassword ? <i className="far fa-eye"></i> : <i className="far fa-eye-slash"></i>}
-            </span>
-        </div>
-        <span className="text-red-500 text-sm mt-0 mb-4">{errors.password?.message}</span>
+//         <label className='uppercase text-[#49A078] text-md'>Password</label>
+//         <div className="relative">
+//           <input className='mb-1 mt-1 p-3 outline-1 outline-[#263228] outline rounded-sm pr-5 focus:outline-[#49A078] focus:outline-2'
+//             {...register("password")} placeholder="Password" type={showPassword ? "text": "password"}/>
+//             <span onClick={() => setShowPassword(!showPassword)}
+//             className="cursor-pointer absolute right-2 top-4 text-[#263228]">
+//               {/* Font Awesome Eye Icon */}
+//               {showPassword ? <i className="far fa-eye"></i> : <i className="far fa-eye-slash"></i>}
+//             </span>
+//         </div>
+//         <span className="text-red-500 text-sm mt-0 mb-4">{errors.password?.message}</span>
 
-        <button className="bg-[#263228] text-white p-3 mt-3 hover:text-[#263228] border-2
-         hover:bg-white border-[#263228] font-bold authentication-button" type="submit">
-          Register
-        </button>
-      </form>
+//         <button className="bg-[#263228] text-white p-3 mt-3 hover:text-[#263228] border-2
+//          hover:bg-white border-[#263228] font-bold authentication-button" type="submit">
+//           Register
+//         </button>
+//       </form>
 
-      {/* OAuth buttons */}
-      <div className="flex items-center justify-center p-4">
-        {/* Google OAuth */}
-        {/* You may need to replace this with the appropriate library */}
+//       {/* OAuth buttons */}
+//       <div className="flex items-center justify-center p-4">
+//         {/* Google OAuth */}
+//         {/* You may need to replace this with the appropriate library */}
         
 
-        {/* Apple OAuth */}
-        {/* Add Apple OAuth button here */}
-      </div>
-    </div>
-  );
-}
+//         {/* Apple OAuth */}
+//         {/* Add Apple OAuth button here */}
+//       </div>
+//     </div>
+//   );
+// }
 
-export default RegisterForm;
+// export default RegisterForm;
+
 
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,7 +1,9 @@
 import { AuthTitle } from "../components/Authentication";
-import { RegisterForm }from "../components/Authentication";
 import { LINKS } from "../components/constants";
 import { useNavigate } from "react-router-dom";
+import { supabase } from "../supabase/supabaseClient";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from '@supabase/auth-ui-shared';
 
 const Register = () => {
   const navigate = useNavigate();
@@ -10,19 +12,73 @@ const Register = () => {
     navigate(LINKS.LOGIN);
   }
 
-  return (
-    <div className="flex flex-col h-screen justify-center items-center">
-      <AuthTitle title="Trace" subTitle="Register" />
+  // return (
+  //   <div className="flex flex-col h-screen justify-center items-center">
+  //     <AuthTitle title="Trace" subTitle="Register" />
       
-      <RegisterForm/>
+  //     {/* Bottom link */}
+  //     <div className="mt-auto">
+  //       <p className="text-center text-[#49A078] uppercase cursor-pointer"
+  //        onClick={goToLogin}>Already have an account?</p>
+  //     </div>
+  //   </div>
+  // );
 
-      {/* Bottom link */}
-      <div className="mt-auto">
-        <p className="text-center text-[#49A078] uppercase cursor-pointer"
-         onClick={goToLogin}>Already have an account?</p>
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold mb-4 text-[#263228]">Trace</h1>
+      <div className="max-w-md w-full">
+        <Auth supabaseClient={supabase}
+          appearance={{ theme: ThemeSupa,
+          variables: {
+            default : {
+              colors: {
+                brandAccent: '#49A078',
+                brandButtonText: '#263228',
+                defaultButtonText: '#263228',
+                defaultButtonBorder: '#4d6551',
+                defaultButtonBackgroundHover: '#b7c8b9',
+                inputBorder: '#263228',
+                dividerBackground: '#444444',
+                inputBorderFocus: '#49A078',
+                inputBorderHover: '#49A078',
+                anchorTextColor: '#49A078',
+                anchorTextHoverColor: '#49A078'
+              },
+              fontSizes: {
+                baseBodySize: '16px',
+                baseInputSize: '18px',
+                baseLabelSize: '18px',
+                baseButtonSize: '18px',
+              },
+              fonts: {
+                labelFontFamily: 'Outfit',
+                inputFontFamily: 'Outfit',
+                buttonFontFamily: 'Outfit'
+              }
+            }
+          }}}
+          localization={{
+            variables: {
+              sign_in: {
+                email_label: 'Email',
+                password_label: 'Password',
+                email_input_placeholder: 'example@example.com'
+              },
+              sign_up: {
+                email_label: 'Email',
+                password_label: 'Password',
+              }
+            }
+          }}
+          providers={["google"]}
+        />
       </div>
+      
     </div>
-  );
+  )
+
+
 };
 
 export default Register;
