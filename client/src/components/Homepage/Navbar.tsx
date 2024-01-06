3d9fbfe5bdf1b0cb548776f4714b719d38cd4c93
Zhi <zjchen402@gmail.com>
2024-01-06 14:52:18 +0000

    refactor: account components go into separate files.

--- client/src/components/Homepage/Navbar.tsx
+++ client/src/components/Homepage/Navbar.tsx
@@ -1,21 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 import { useNavigate } from "react-router-dom";
 import { LINKS } from "../constants";
-
-type NavbarItemProps = {
-  title: string,
-  classprops: string
-};
-
-const NavbarItem = ({title, classprops} : NavbarItemProps) => (
-  <li className={`mx-4 cursor-pointer font-bold ${classprops}`}>{title}</li>
-)
+import { supabase } from "../../supabase/supabaseClient";
+import { Session } from "@supabase/supabase-js";
 
 const Navbar = () => {
   const navigate = useNavigate();
   const [toggleMenu, setToggleMenu] = useState(false);
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    })
+
+    return () => subscription.unsubscribe()
+  }, []);
 
   const goToLogin = () => {
     navigate(LINKS.LOGIN);
@@ -27,7 +35,12 @@ const Navbar = () => {
 
   const goToAccount = () => {
     navigate(LINKS.ACCOUNT);
-  }
+  };
+
+  const handleLogout = () => {
+    supabase.auth.signOut();
+    navigate(LINKS.HOMEPAGE);
+  };
 
   return (
     <nav className="flex bg-[#1f2421] items-center p-5 m-auto relative flex-initial">
@@ -46,9 +59,16 @@ const Navbar = () => {
           <li className="mx-4 cursor-pointer font-bold px-3 hover:underline hover:underline-offset-4" onClick={goToAccount}>
             Profile
           </li>
-          <li className="px-8 bg-[#49A078] py-2 rounded-md cursor-pointer font-bold" onClick={goToLogin}>
-            Login
-          </li>
+          {!session ? (
+            <li className="px-8 bg-[#49A078] py-2 rounded-md cursor-pointer font-bold" onClick={goToLogin}>
+              Login
+            </li>
+          ) : (
+            <button className="px-8 bg-[#49A078] py-2 rounded-md cursor-pointer font-bold text-center md:text-left md:mr-2"
+              onClick={handleLogout}>
+              Sign Out
+            </button>
+          )}
         </ul>
         <div className="flex relative ml-auto md:hidden">
           {!toggleMenu && (
@@ -71,11 +91,18 @@ const Navbar = () => {
               <li className="mx-4 cursor-pointer font-bold text-xl pb-5" onClick={goToAccount}>
                 Profile
               </li>
-              <li className="mx-4 text-xl cursor-pointer font-bold text-[#49A078]" onClick={goToHomepage}>
-                Login
-              </li>
-            </ul>
-          )}
+              {!session ? (
+                <li className="px-4 cursor-pointer font-bold text-xl" onClick={goToLogin}>
+                  Login
+                </li>
+              ) : (
+                <button className="px-4 cursor-pointer font-bold md:text-left md:mr-2"
+                  onClick={handleLogout}>
+                  Sign Out
+                </button>
+              )}
+                </ul>
+              )}
         </div> 
       </div>
     </nav>
--- client/src/pages/Account.tsx
+++ client/src/pages/Account.tsx
@@ -1,119 +1,14 @@
-import { useNavigate } from "react-router-dom";
-import { supabase } from "../supabase/supabaseClient";
-import { LINKS } from "../components/constants";
-import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
 import { Navbar } from "../components/Homepage";
+import { AccountProfile } from "../components/Account";
 
 const Account = () => {
-  const navigate = useNavigate();
-  const location = useLocation()
-  const session = location.state?.session;
-  
-  const [fullName, setFullName] = useState(null);
-  const [age, setAge] = useState(null);
-  const [avatarUrl, setAvatarUrl] = useState("");
-  const [bio, setBio] = useState(null);
-  const [passedAvatarUrl, setPassedAvatarUrl] = useState("");
-
-  const goToEditAccountPage = () => {
-    navigate(LINKS.EDIT_ACCOUNT, { state: { session, fullName, age, passedAvatarUrl, bio }});
-  }
-
-  async function downloadImage(path : string) {
-    try {
-      setPassedAvatarUrl(path);
-      const { data, error } = await supabase.storage.from('avatars').download(path);
-      if (error) {
-        throw error;
-      }
-      const url = URL.createObjectURL(data);
-      setAvatarUrl(url);
-    } catch (error) {
-      console.log('Error downloading image: ', error);
-    }
-  }
-
-  useEffect(() => {
-    let ignore = false;
-    // Go back to auth page if not login
-    if (!session) {
-      navigate(LINKS.LOGIN);
-    }
-
-    async function getProfile() {
-      const { data, error } = await supabase
-        .from('profiles')
-        .select("full_name, avatar_url, age, bio")
-        .eq('id', session?.user.id)
-        .single();
-
-      // if data from database is null, user is newly joined
-      if (data?.full_name === null) {
-        goToEditAccountPage();
-      }
-
-      if (error) {
-        console.warn(error);
-      } else if (data) {
-        setFullName(data.full_name);
-        setAge(data.age);
-        downloadImage(data.avatar_url);
-        setBio(data.bio);
-      }
-    }
-
-    getProfile();
-
-    return () => {
-      ignore = true;
-    }
-  });
-
-  const handleLogout = () => {
-    supabase.auth.signOut();
-    navigate(LINKS.HOMEPAGE);
-  }
-
   return (
     <div>
-      <Navbar/>
-      <div className="container mx-auto p-4">
-        <div className="flex flex-wrap">
-          {/* Account Profile Div */}
-          <div className="w-full mb-4 sm:flex-col">
-            <div className="flex flex-col md:flex-row items-start">
-              <img className="w-48 h-48 rounded-full mb-4 md:mr-4 md:mb-0" src={avatarUrl} alt="Profile Picture" />
-              <div>
-                <h1 className="text-3xl font-bold text-[#1f2421]">{fullName}</h1>
-                <h2 className="text-md text-gray-600">{age}</h2>
-                <p className="lg:text-xl text-lg mt-2 text-gray-700">
-                {bio}
-                </p>
-              </div>
-            </div>
-            <button className="ml-auto px-8 bg-[#49A078] py-2 rounded-md cursor-pointer font-bold text-center mt-4 text-white"
-              onClick={goToEditAccountPage}>
-              Edit Profile
-            </button>
-          </div>
-        </div>
-
-        <div className="w-full mt-4">
-          <h1 className="text-3xl font-bold text-[#1f2421]">Previous Experience</h1>
-          {/* Add your work experience content here */}
-        </div>
-
-        <button className="px-8 bg-[#49A078] py-2 rounded-md cursor-pointer font-bold text-center md:text-left md:mr-2"
-          onClick={handleLogout}>
-          Sign Out
-        </button>
-      </div>
+      <Navbar />
+      <AccountProfile />
     </div>
   );
 };
 
-// default icon
-{/* <a href="https://www.flaticon.com/free-icons/user" title="user icons">User icons created by Becris - Flaticon</a> */}
 
 export default Account;
